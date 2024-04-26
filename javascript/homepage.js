e114cddab5eb0d5deef12ce4bcf9a19fa32015d4
EPhill98 <euan.98@live.co.uk>
2024-04-26 10:41:25 +0800

    Branch start

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -4,16 +4,12 @@ const promotionMessages = [
     "DISNEY PREMIER ACCESS - 1-ATTRACTION, STARTING FROM HK $79"
 ];
 
-// Get the reference to the element with ID "msg"
 const msg = document.getElementById("message");
 
-// Initialize flag to false
 let flag = false;
 
-// Initialize rand
 let rand = Math.floor(Math.random() * 3);
 
-// Assign a random message from the promotionMessages array to the textContent of the "msg" element
 var currMsg = promotionMessages[rand];
 msg.innerText = currMsg;
 
@@ -43,19 +39,13 @@ function nonEmpty(){
     var f3 = document.getElementById("noVisitors").value.trim();
     var warning = document.getElementById('warning');
     if (f1 == '' || f2 == '' || f3 == ''){
-        alert("INPUT");
         warning.style.display = 'block';
         return false;
-    } else {
-        alert("TRUE");
-        return true;
-    }
+    } 
 }
 
 //Functional calls
 setInterval(changeMsg, 3000);
-// Wait for the DOM content to be loaded
 document.addEventListener('DOMContentLoaded', function () {
-    // Event listener to switch videos when the current one ends
     document.getElementById('videoCircle').addEventListener('ended', switchVideo);
 });
